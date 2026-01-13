65c3e725acc4f685cbc83648c93c51b7f5e852f1
Ilian Shchepinskyi <iliantheone@gmail.com>
2026-01-13 18:50:32 +0200

    Completed the task

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,8 +4,19 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 public class IndexModel : PageModel
 {
+    public string UserName { get; set; }
+    public DateOnly BirthDate { get; set; }
+    public int UserAge { get; set; }
+    public bool IsAdmin { get; set; }
+    public DateTime CreationTime { get; set; }
+
     public void OnGet()
     {
-
+        UserName = "John Doe";
+        BirthDate = new DateOnly(1990, 5, 15);
+        UserAge = DateTime.Now.Year - BirthDate.Year;
+        if (BirthDate > DateOnly.FromDateTime(DateTime.Now).AddYears(-UserAge)) { UserAge--; }
+        IsAdmin = true;
+        CreationTime = DateTime.Now;
     }
 }
